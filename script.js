3616c693fda4e8086e57af09d61491b000623632
Ayush1M <worklife2442@gmail.com>
2024-03-07 20:03:03 +0530

    added copied to clipboard functionality and updated styling.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorPicker = document.getElementById("color-picker")
 const colorValue = document.getElementById("color-value")
 const formData = document.getElementById("form-data")
 const colors = document.querySelector(".colors")
-
+ 
 
 formData.addEventListener("submit", (e)=>{
     e.preventDefault()
@@ -20,5 +20,19 @@ formData.addEventListener("submit", (e)=>{
         document.getElementById(`color-${index+1}`).style.backgroundColor = hex
         document.getElementById(`text-${index+1}`).textContent = hex
     }))
+})
+
+
+document.querySelectorAll(".text").forEach((copy) =>{
+    copy.addEventListener("click" ,(e)=>{
+        const hex = e.target.textContent
+        navigator.clipboard.writeText(hex)
+
+        const copyHex = document.createElement("div")
+        copyHex.classList.add("copy-div")
+        copyHex.textContent = "copied to clipboard"
+        e.target.appendChild(copyHex)
+        setTimeout(()=> e.target.removeChild(copyHex), 500)
+    })
 })
 
